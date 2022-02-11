6fc62606607a8865873aa271a587ff1c1c809b1c
antonio357 <carlosalbuquerque359@gmail.com>
2022-02-11 17:15:10 -0300

    how to create functions

---
+++ clojure-basics/src/clojure_basics/functions.clj
@@ -0,0 +1,19 @@
+;; defn to create functions
+;; (defn function-name
+;;   [parameters]
+;;   ;; return
+;; )
+
+(defn sum
+  [a, b]
+  (+ a b)
+)
+
+(sum 1 2)
+
+(defn pow2
+  [num]
+  (* num num)
+)
+
+(pow2 2)
