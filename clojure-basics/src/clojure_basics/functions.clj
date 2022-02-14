d5d2f071353cbe9d3577aa24c55f7a8020b5908b
antonio357 <carlosalbuquerque359@gmail.com>
2022-02-14 15:05:29 -0300

    functions, recursion lambda functions

--- clojure-basics/src/clojure_basics/functions.clj
+++ clojure-basics/src/clojure_basics/functions.clj
@@ -1,17 +1,14 @@
 ;; defn to create functions
-;; (defn function-name
-;;   [parameters]
-;;   ;; return
+;; (defn function-name [parameters]
+;;  return
 ;; )
 
-(defn sum
-  [a b]
+(defn sum [a b]
   (+ a b)
 )
 (sum 1 2)
 
-(defn pow2
-  [num]
+(defn pow2 [num]
   (* num num)
 )
 (pow2 2)
@@ -20,6 +17,17 @@
 (def sub (fn [a b] (- a b)))
 (sub 1 2)
 
-;; use ifn? to check if anything is a function 
+;; use ifn? to check if anything is a function that can be called
 (ifn? sum)
 (ifn? pow2)
+
+;; recursion
+
+;; in standard functions
+(defn itsPowerOf2 [num] 
+  (if (> num 2) (itsPowerOf2 (/ num 2)) (if (== num 2) true false))
+)
+(itsPowerOf2 16)
+
+;; in anonymous functions we use recur since we hava no name to call
+((fn [num] (if (> num 2) (recur (/ num 2)) (if (== num 2) true false))) 8)
