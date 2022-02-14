2ff228abf970a49328aafbf0e636ce77198f288b
antonio357 <carlosalbuquerque359@gmail.com>
2022-02-14 13:59:43 -0300

    functions

--- clojure-basics/src/clojure_basics/functions.clj
+++ clojure-basics/src/clojure_basics/functions.clj
@@ -8,12 +8,18 @@
   [a b]
   (+ a b)
 )
-
 (sum 1 2)
 
 (defn pow2
   [num]
   (* num num)
 )
-
 (pow2 2)
+
+;; anonimous functions can be created with fn
+(def sub (fn [a b] (- a b)))
+(sub 1 2)
+
+;; use ifn? to check if anything is a function 
+(ifn? sum)
+(ifn? pow2)
