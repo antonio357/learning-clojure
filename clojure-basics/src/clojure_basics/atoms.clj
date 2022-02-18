590176fce036ff3dadc296af6a1a576d46f842e6
antonio357 <carlosalbuquerque359@gmail.com>
2022-02-18 17:12:50 -0300

    atoms, data structs to solve side effects

---
+++ clojure-basics/src/clojure_basics/atoms.clj
@@ -0,0 +1,5 @@
+; atoms are a solution to side effects
+; atoms are the closest thing from variables in clojure since the data structs in clojure are immutable (they threes with nodes and the leafs are values)
+; atom is a memory information with u can throw a function inside and the function will take the atom as argument and make some processing without changing the atom at the end of the function execution the function will handle a new value to the atom so it can update its value
+; ok so if u have more than one function thrown in the atom both will execute in parallel on them will change deliver the new value for atom first then the second function will be notified that the atom was updated so it will execute agin with the new value from the atom
+; in this way we avoid lockers and deadlocks and still execute parallelim
