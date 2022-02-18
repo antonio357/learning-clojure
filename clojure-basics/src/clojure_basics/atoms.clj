25a22dbd4ad2df35a89d68a10f9de3bfa7f3a9f2
antonio357 <carlosalbuquerque359@gmail.com>
2022-02-18 17:15:13 -0300

    atoms

--- clojure-basics/src/clojure_basics/atoms.clj
+++ clojure-basics/src/clojure_basics/atoms.clj
@@ -3,4 +3,4 @@
 ; atoms are the closest thing from variables in clojure since the data structs in clojure are immutable (they threes with nodes and the leafs are values)
 ; atom is a memory information with u can throw a function inside and the function will take the atom as argument and make some processing without changing the atom at the end of the function execution the function will handle a new value to the atom so it can update its value
 ; ok so if u have more than one function thrown in the atom both will execute in parallel on them will change deliver the new value for atom first then the second function will be notified that the atom was updated so it will execute agin with the new value from the atom
-; in this way we avoid lockers and deadlocks and still execute parallelim
+; in this way we avoid lockers and deadlocks and still execute parallelism
