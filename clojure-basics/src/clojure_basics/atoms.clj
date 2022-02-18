0dd7f9cc49073996e1cea01cc086500752ca9e4b
antonio357 <carlosalbuquerque359@gmail.com>
2022-02-18 17:13:58 -0300

    atoms link

--- clojure-basics/src/clojure_basics/atoms.clj
+++ clojure-basics/src/clojure_basics/atoms.clj
@@ -1,3 +1,4 @@
+; check all the explanation https://www.youtube.com/watch?v=0if71HOyVjY&ab_channel=GOTOConferences
 ; atoms are a solution to side effects
 ; atoms are the closest thing from variables in clojure since the data structs in clojure are immutable (they threes with nodes and the leafs are values)
 ; atom is a memory information with u can throw a function inside and the function will take the atom as argument and make some processing without changing the atom at the end of the function execution the function will handle a new value to the atom so it can update its value
