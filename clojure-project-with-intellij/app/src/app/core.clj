fe95c0d7f2b112f25f776dbc49f21e395c131830
antonio357 <carlosalbuquerque359@gmail.com>
2022-02-22 17:51:31 -0300

    clojure with intellij

---
+++ clojure-project-with-intellij/app/src/app/core.clj
@@ -0,0 +1,8 @@
+(ns app.core)
+
+; consumir a api https://raw.githubusercontent.com/jeffreylancaster/game-of-thrones/master/data/characters.json
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
