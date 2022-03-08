7617d8e4138bccfc65c3534557ad08a3c21ed168
antonio357 <carlosalbuquerque359@gmail.com>
2022-03-08 17:24:19 -0300

    check point

--- clojure-project-with-intellij/app/src/app/get_screen_time.clj
+++ clojure-project-with-intellij/app/src/app/get_screen_time.clj
@@ -2,22 +2,25 @@
   (:require [app.load_data :refer [get-data]])
   (:require [app.time :refer [get-time-interval]]))
 
+(def ^:private db (get-data))
+
 (defn print-screen-time [screen-time]
   (map #(prn % (% screen-time)) (keys screen-time)))
 
-(defn get-characters-from-scene [scene]
+; with atoms
+; now it works the problem was the internal maps returned empty collections with just nil inside so clojure interpred as lazy sqe and it woul only execute code that returns lazy seq if it will use the values
+
+(defn- get-characters-from-scene [scene]
   (->> (:characters scene)
        (map :name)
        (set)
        (vec)))
 
-(defn get-episodes [db]
+(defn- get-episodes [db]
   (->> (:episodes db)
        (:data)
        (:episodes)))
 
-; with atoms
-; now it works the problem was the internal maps returned empty collections with just nil inside so clojure interpred as lazy sqe and it woul only execute code that returns lazy seq if it will use the values
 (defn- get-screen-time [db atom-map]
   (->> (get-episodes db)
        (map (fn [episode] (doall (as-> (:scenes episode) scenes
@@ -30,10 +33,23 @@
                                                                                    (swap! atom-map #(assoc % character (+ (character %) @screen-time)))
                                                                                    (swap! atom-map #(assoc % character @screen-time))))) @characters))) scenes)))))))
 
-(def db (get-data))
 (def ^:private screen-time (atom {}))
 (get-screen-time db screen-time)
 (print-screen-time @screen-time)
 
 
 ;better way to do, no atoms
+(defn- get-json-data [db json-name]
+  (let [json-name (keyword json-name)] (-> (json-name db)
+                                           (:data)
+                                           (json-name))))
+
+(defn- get-characters [db]
+  (->> (get-json-data db "characters")
+       (map #(:characterName %))
+       (set)
+       (vec)))
+
+; return :character-name screen-time
+(defn- get-character-screen-time [character, episodes]
+  (map [] episodes))
